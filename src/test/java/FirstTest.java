975f626508703cc337f83294896024952f2b7bbc
ilzam <ilzammulkhaq85@gmail.com>
2022-10-16 20:54:34 +0700

    feat: add unit test

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FirstTest {
+
+    @Test
+    public void assertAddEqual() {
+
+        assertEquals(1, 1);
+
+    }
+
+}
