d4543e0c61738ecd164270c26d1df571d6203ad3
ilzam <ilzammulkhaq85@gmail.com>
2022-10-17 16:27:19 +0700

    feat: add function to detect is wall

--- src/lumpat/Main.java
+++ src/lumpat/Main.java
@@ -10,9 +10,23 @@ public class Main {
         p = sc.nextInt();
         l = sc.nextInt();
 
+        char[][] m = new char[p][l];
+
+        for(int i = 0; i < p; i++) {
+            m[i] = sc.nextLine().split("").toCharArray();
+        }
+
+
+
         System.out.println(p + " " + l);
 
         sc.close();
     }
+
+    public static boolean isHWall(char[] p) {
+        return  !Arrays.asList(p).stream().anyMatch(x -> x == " ");
+    }
+
+    
 
 }
