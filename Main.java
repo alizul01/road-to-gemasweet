eec13a22adab737e2981bb0cfa6a50a71689ec37
Ali Zulfikar <alizulfikar032@gmail.com>
2022-10-19 14:21:17 +0700

    SOLVED : hitungpenjumlahan
    Co-authored-by: Tio Misbaqul Irawan <2141720003@student.polinema.ac.id>

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -6,6 +7,47 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(30);
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int Q = sc.nextInt();
+
+        int arr[][] = new int[N][M];
+        int res [] = new int[Q];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < Q; i++) {
+            res[i] = calculate(
+                arr,
+                sc.nextInt() - 1,
+                sc.nextInt() - 1,
+                sc.nextInt() - 1,
+                sc.nextInt() - 1
+            );
+        }
+
+        for (int i : res) {
+            System.out.println(i);
+        }
+        
+        System.out.println();
+    }
+
+    public static int calculate(int[][] m, int x1, int y1, int x2, int y2) {
+        int res = 0;
+
+        for (int i = x1; i <= x2; i++) {
+            for (int j = y1; j <= y2; j++) {
+                res += m[i][j];
+            }
+        }
+
+        return res;
     }
 }
