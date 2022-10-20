2e1468330df54f5b1c125f173f0f0b285ff8e6f1
Ali Zulfikar <alizulfikar032@gmail.com>
2022-10-20 15:39:22 +0700

    SOLVED : Laser Iron Man PINDAH FILE 2020 2021
    Co-authored-by: Cak Suku <ilzammulkhaq85@gmail.com>
    Co-authored-by: argyaWicaksana <argyaWicaksana@users.noreply.github.com>

---
+++ src/2021/laserironman/Main.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] ironmanPos = new int[2];
+
+        ironmanPos[0] = sc.nextInt(); // posisi X dari ironman
+        ironmanPos[1] = sc.nextInt(); // posisi y dari ironman
+
+        int res = 0;
+        int N = sc.nextInt(); // jumlah ultron
+
+        int[][] ultrons = new int[N][2]; // koordinat ultron
+
+        for (int i = 0; i < N; i++) { // i => ultron ke berapa?
+            ultrons[i][0] = sc.nextInt(); // titik x
+            ultrons[i][1] = sc.nextInt(); // titik y
+        }
+
+        int K = sc.nextInt();
+
+        int[][] obstacle = new int[K][4];
+
+        for (int i = 0; i < K; i++) {
+            obstacle[i][0] = sc.nextInt();
+            obstacle[i][1] = sc.nextInt();
+            obstacle[i][2] = sc.nextInt();
+            obstacle[i][3] = sc.nextInt();
+        } // input obstacle
+
+        // for (int i = 0; i < obstacle.length; i++) {
+        // System.out.println(Arrays.toString(obstacle[i]));
+        // } debug obstacle
+
+        // System.out.println(isIntersecting(new int[] { 5, 8 }, new int[] { 9, 4 }, new int[] { 7, 5 }, new int[] { 8, 5 }));
+
+        for (int i = 0; i < ultrons.length; i++) {
+            int hit = 1;
+            for (int j = 0; j < obstacle.length; j++) {
+                if (isIntersecting(ironmanPos, ultrons[i], new int [] {obstacle[j][0], obstacle[j][1]}, new int [] {obstacle[j][2], obstacle[j][3]})) {
+                    hit = 0;
+                    break;
+                }
+            }
+            if (hit == 1) System.out.println("Posisi ultron ke-" + i + " " + ultrons[i][0] + " " + ultrons[i][1]);
+            res += hit;
+        }
+
+        System.out.println(res);
+
+        sc.close();
+    }
+
+    public static boolean isPerpendicularIntersection(int[] ironmanPos, int[] ultronPos, int[] obs1, int[] obs2) {
+        if ((obs1[0] < ironmanPos[0] && obs1[0] > ultronPos[0])
+                || (obs1[1] < ironmanPos[1] && obs1[1] > ironmanPos[1])) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public static boolean isIntersecting(int ironmanPos[], int ultronPos[], int obs1[], int obs2[]) {
+        double denominator = ((ironmanPos[0] - ultronPos[0]) * (obs1[1] - obs2[1]))
+                - ((ironmanPos[1] - ultronPos[1]) * (obs1[0] - obs2[0]));
+        if (denominator == 0)
+            return false;
+
+        double t = (((ironmanPos[0] - obs1[0]) * (ironmanPos[1] - ultronPos[1]))
+                - ((ironmanPos[1] - obs1[1]) * (ironmanPos[0] - ultronPos[0]))) / denominator;
+        double u = (((ironmanPos[0] - obs1[0]) * (obs1[1] - obs2[1]))
+                - ((ironmanPos[1] - obs1[1]) * (obs1[0] - obs2[0]))) / denominator;
+
+        if ((t >= 0 && t <= 1) && (u >= 0 && u <= 1))
+            return true;
+        return false;
+    }
+}
